9a3df100c307e58d29c81994e4743624e7533c09
EunSeongJang <105621512+EunSeongJang@users.noreply.github.com>
2022-06-17 21:54:57 +0900

    2022.06.17 - 함수 표현식 (화살표 함수, 이벤트, 이벤트 처리기) - 자바스크립트 객체 (프로퍼티, 메서드, 배열, Date, Math, 브라우저 관련 객체) - 문서객체모델 (DOM,  DOM요소에 접근하기)

---
+++ src/15/js/event2.js
@@ -0,0 +1,23 @@
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+
+//camel 표기법 사용하면 안 먹힘
+btn1.onclick = () => {
+    box1.classList.toggle("box-resize");
+    box1.classList.toggle("box-bg-change");
+}
+
+btn2.onclick = () => {
+    box2.classList.toggle("box-resize");
+    box2.classList.toggle("box-bg-change");
+}
+
+box1.onmouseover = () => {
+    box1.classList.toggle("box-bg-change");
+}
+
+box1.onmouseout = () => {
+    box1.classList.toggle("box-bg-change");
+}
